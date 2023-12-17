6ea023688e2df4b918dfe2dbe57929faec7e8651
kowsisakthi <124908474+kowsisakthi@users.noreply.github.com>
2023-12-17 20:01:07 +0530

    Add files via upload

--- kowsi_user_module/src/page/Exam.js
+++ kowsi_user_module/src/page/Exam.js
@@ -5,6 +5,7 @@ function Exam() {
   const examId = sessionStorage.getItem("exam");
   const [questions, setQuestions] = useState([]);
   const [answers, setAnswers] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // Check if examId is truthy before making the fetch request
@@ -22,64 +23,101 @@ function Exam() {
       })
         .then((res) => res.json())
         .then((data) => {
-          console.log("data----->"+data.question.examquestion);
+          console.log(data.question.examquestion);
           setQuestions(data.question.examquestion);
+          setLoading(false); // Set loading to false after fetching data
         })
         .catch((error) => {
           console.error('Error fetching questions:', error);
+          setLoading(false); // Set loading to false in case of an error
         });
     }
-  }, [examId]);
+  }, []);
 
   const handleOptionChange = (questionId, selectedOption) => {
     setAnswers((prevAnswers) => ({
       ...prevAnswers,
-      [questionId]: selectedOption,
+      [questionId]: [...(prevAnswers[questionId] || []), selectedOption],
+
     }));
   };
 
   return (
     <div className='container mt-4'>
-      <h2>Exam Page</h2>
+      {questions.map((quesGroup) => (
+        <React.Fragment>
+          {quesGroup.map((re) => (
+            <div key={re.questionId} className='card mt-3'>
+              <div className='card-body'>
+                <h5 className='card-title'>{re.questionDetail}</h5>
+                <div className='form-check'>
+                  {['A', 'B', 'C', 'D', 'E'].map((option) => {
+                    const optionKey = `option${option}`;
+                    const optionValue = re[optionKey];
+                    const que = re.QuestionType;
+                    console.log("----------->" + que);
+                    switch (que) {
+                      case 'QT_SC':
+                        console.log("-------->single");
+                        return optionValue && (
 
-      {/* {questions.map((question) => (
-        <div key={question.id} className='card mt-3'>
-          <div className='card-body'>
-            <h5 className='card-title'>{question.question}</h5>
+                          <div key={optionKey} className='form-check'>
+                            <input
+                              type='radio'
+                              className='form-check-input'
+                              id={`${optionKey}-${re.questionId}`}
+                              name={`question${re.questionId}`}
+                              value={optionKey}
+                              onChange={() => handleOptionChange(re.questionId, optionKey)}
+                              checked={answers[re.questionId] === optionKey}
+                            />
+                            <label className='form-check-label' htmlFor={`${optionKey}-${re.questionId}`}>
+                              {optionValue}
+                            </label>
+                          </div>
+                        );
+                      case 'QT_MC':
+                        console.log("-------->multiple");
+                        // Render checkboxes for multiple-choice question
+                        return optionValue && (
+                          <div key={optionKey} className='form-check'>
+                            <input
+                              type='checkbox'
+                              className='form-check-input'
+                              id={`${optionKey}-${re.questionId}`}
+                              name={`question${re.questionId}`}
+                              value={optionKey}
+                              onChange={() => handleOptionChange(re.questionId, optionKey)}
+                              checked={answers[re.questionId] && answers[re.questionId].includes(optionKey)}
+                            />
+                            <label className='form-check-label' htmlFor={`${optionKey}-${re.questionId}`}>
+                              {optionValue}
+                            </label>
+                          </div>
+                        );
+                    }
 
-            <div className='form-check'>
-              {question.options.map((option, index) => (
-                <div key={index} className='form-check'>
-                  <input
-                    type='radio'
-                    className='form-check-input'
-                    id={`option${index}`}
-                    name={`question${question.id}`}
-                    value={option}
-                    onChange={() => handleOptionChange(question.id, option)}
-                    checked={answers[question.id] === option}
-                  />
-                  <label className='form-check-label' htmlFor={`option${index}`}>
-                    {option}
-                  </label>
+                  })}
                 </div>
-              ))}
+              </div>
             </div>
-          </div>
-        </div>
-      ))} */}
+          ))}
+        </React.Fragment>
+      ))}
 
-      <div className='mt-4'>
-        <button
-          className='btn btn-primary'
-          onClick={() => {
-            // Handle submission logic here, e.g., sending answers to a server
-            console.log('Submitted Answers:', answers);
-          }}
-        >
-          Submit
-        </button>
-      </div>
+      {!loading && (
+        <div className='mt-4'>
+          <button
+            className='btn btn-primary'
+            onClick={() => {
+              // Handle submission logic here, e.g., sending answers to a server
+              console.log('Submitted Answers:', answers);
+            }}
+          >
+            Submit
+          </button>
+        </div>
+      )}
     </div>
   );
 }
