d1edfc250a5bf11a267add8fb6c13426b28fa07e
V-Malar <128116398+V-Malar@users.noreply.github.com>
2023-12-21 18:09:13 +0530

    Add files via upload

---
+++ malar/src/App.js
@@ -0,0 +1,18 @@
+import AdminDashboard from "./Components/AdminComponent/AdminDashboard/AdminDashboard";
+import Example from "./Components/AdminComponent/AdminDashboard/ExamComponent/ModalSample";
+
+
+function App() {
+  return (
+    <div>
+      <AdminDashboard />
+      {/* <StudentDashboard/> */}
+      {/* <Example/> */}
+    </div>
+  );
+
+}
+
+
+
+export default App;
